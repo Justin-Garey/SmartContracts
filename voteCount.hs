15a9ee70f13590f109c1872d875f9cca7f618cd9
Justin Garey <jg390518@ohio.edu>
2021-10-05 14:50:31 -0400

    Added text file functionality and better output readability

--- voteCount.hs
+++ voteCount.hs
@@ -1,27 +1,23 @@
+import System.Directory -- Check if file exists with doesFileExist
+import System.Environment -- Gives us getArgs
+import System.IO -- For working with input
 import Data.List
-data Vote = Yes | No | Abstain deriving ( Eq )
 
-testVotes = [Yes, Yes, No, Abstain]
-testMultiChoice = "Abe George George Abe George Kennedy Kennedy Kennedy Kennedy"
-
-countVotes :: [Vote] -> String
-countVotes votes = show yesPercent ++ "% voted yes. " ++ show noPercent ++ "% voted no. " ++ show abstainPercent ++ "% chose to abstain."
-    where yesPercent = fromIntegral(length(filter (== Yes) votes)) / fromIntegral(length votes) * 100
-          noPercent = fromIntegral(length(filter (== No) votes)) / fromIntegral(length votes) * 100
-          abstainPercent = fromIntegral(length(filter (== Abstain) votes)) / fromIntegral(length votes) * 100
-
-countMultiChoice :: String -> [Int] -- Function to call on string of votes
-countMultiChoice votes = countChoices (words votes)
-
--- Helper function for countMultiChoice
--- Turns a list of strings into a list of ints representing the
--- number of duplicates of each string
--- ["Abe", "George", "George", "George", "John", "John"] -> [1, 3, 2]
-countChoices :: [String] -> [Int] 
+-- countChoices
+-- Takes a list of strings
+-- Returns a list of counted values
+countChoices :: (Eq a) => [a] -> [Int] 
 countChoices votes = case votes of
     [] -> [0]
     (x:xs) -> case partition (==x) votes of
         (y, []) -> [length y]
         (y, ys) -> (length y: countChoices ys)
 
-main = print(countMultiChoice testMultiChoice)
+-- main - runs on program call
+main = do
+    (fileN : _) <- getArgs
+    fileExists <- doesFileExist fileN
+    if fileExists
+        then do contents <- readFile fileN
+                print (zip (nub $ words contents) (countChoices $ words contents))
+        else do putStrLn "The file does not exist"
