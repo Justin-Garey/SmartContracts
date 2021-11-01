8db280c099ae9a1a2d1b6740d481cf746bb1403d
Justin Garey <jg390518@ohio.edu>
2021-11-01 12:27:03 -0400

    Made some clarities in the code for what is being imported and types in main function

--- voteCount.hs
+++ voteCount.hs
@@ -1,7 +1,7 @@
-import System.Directory -- Check if file exists with doesFileExist
-import System.Environment -- Gives us getArgs
-import System.IO -- For working with input
-import Data.List
+import System.Directory ( doesFileExist ) -- Check if file exists with doesFileExist
+import System.Environment ( getArgs ) -- Gives us getArgs
+import System.IO () -- For working with input
+import Data.List ( nub, partition ) 
 
 -- countChoices
 -- Takes a list of strings
@@ -11,13 +11,14 @@ countChoices votes = case votes of
     [] -> [0]
     (x:xs) -> case partition (==x) votes of
         (y, []) -> [length y]
-        (y, ys) -> (length y: countChoices ys)
+        (y, ys) -> length y : countChoices ys
 
 -- main - runs on program call
+main :: IO ()
 main = do
     (fileN : _) <- getArgs
     fileExists <- doesFileExist fileN
     if fileExists
         then do contents <- readFile fileN
-                print (zip (nub $ words contents) (countChoices $ words contents))
+                print (nub (words contents) `zip` countChoices (words contents))
         else do putStrLn "The file does not exist"
